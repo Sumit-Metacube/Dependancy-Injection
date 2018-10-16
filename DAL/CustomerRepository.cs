2489e0620a3be490cabed3d518e78f6ce6f7f484
todessss <44155179+todessss@users.noreply.github.com>
2018-10-16 18:12:19 +0530

    Add files via upload

---
+++ DAL/CustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DAL
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public List<Customer> GetCustomers()
+        {
+            return new List<Customer>(){
+                new Customer { CustomerId = 1, City = "Jaipur", CustomerName = "Ram" },
+                new Customer { CustomerId = 2, City = "Delhi", CustomerName = "Shyam" },
+                new Customer { CustomerId = 3, City = "Ajmer", CustomerName = "Seeta" },
+                new Customer { CustomerId = 4, City = "Mumbai", CustomerName = "Geeta" },
+            };
+        }
+    }
+}
